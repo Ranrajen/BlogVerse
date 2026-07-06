df667b6549a89c21842fcc15e5cf0abbb9d574f9
Ranrajen <rajenrangari863@gmail.com>
2026-07-06 19:09:06 +0530

    update with form content at new button

--- src/components/newpostmodal/NewPostModal.tsx
+++ src/components/newpostmodal/NewPostModal.tsx
@@ -1,39 +1,53 @@
-import './newPostModal.css'
+import './newPostModal.css';
+import { useState } from 'react';
 
 interface NewPostModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-const NewPostModal = ({
-  isOpen,
-  onClose,
-}: NewPostModalProps) => {
+const NewPostModal = ({ isOpen, onClose }: NewPostModalProps) => {
+
+  
+  // ---------------usestate--------
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   if (!isOpen) return null;
+
 
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal" onClick={(e)=> e.stopPropagation()}>
-
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
           <h2>Create New Post</h2>
 
-          <button
-            className="close-btn"
-            onClick={onClose}
-          >
+          <button className="close-btn" onClick={onClose}>
             ✕
           </button>
         </div>
 
         <div className="modal-body">
+         <div className='form-group'>
+          <label htmlFor="title">Title</label>
+           <input
+           
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter post title"
+          />
+         </div>
 
-          <p>
-            Form coming in the next sprint...
-          </p>
-
+         <div className='form-group'>
+          <label htmlFor="content">Content</label>
+           <textarea
+            rows={8}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Write your blog here..."
+          ></textarea>
+         </div>
         </div>
-
       </div>
     </div>
   );
