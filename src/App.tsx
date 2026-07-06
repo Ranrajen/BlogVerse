f5291f3eb1c8d97938e4138082358d0ad72c33d1
Ranrajen <rajenrangari863@gmail.com>
2026-07-06 09:23:18 +0530

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,43 @@ import './App.css';
 import Header from './components/header/Header';
 import NewPostButton from './components/newpost/NewPostButton';
 import SearchBar from './components/search/SearchBar';
-import ReadingSpace from './components/readingSpace/ReadingSpace'
+import ReadingSpace from './components/readingSpace/ReadingSpace';
 import PostsFeed from './components/postsfeed/PostsFeed';
 
+import { useState } from 'react';
+import type { Post } from './types/Post';
+import { dummyPosts } from './data/dummyPosts';
+
+import NewPostModal from './components/newpostmodal/NewPostModal';
+
+
+
+
 const App = () => {
+  const [selectedPost , setselectedPost] = useState<Post | null>(dummyPosts[0])
+  const [isModalOpen, setIsModalOpen] =
+  useState(false);
   return (
     <div className="app">
       {' '}
       <Header />
       <div className="screen-split">
-        <aside className="left-side">the side panel  
-          <ReadingSpace></ReadingSpace>
+        <aside className="left-side">
+          {/* the side panel */}
+          <ReadingSpace selectedPost={selectedPost}/>
         </aside>
         <main className="right-side">
-          <div className='top-bar'><SearchBar />
-          <NewPostButton /></div>
-         <PostsFeed/>
+          <div className="top-bar">
+            <SearchBar />
+            <NewPostButton onClick={()=>setIsModalOpen(true)} />
+          </div>
+          <PostsFeed onSelectPost={setselectedPost}/>
         </main>
       </div>
+      <NewPostModal
+  isOpen={isModalOpen}
+  onClose={() => setIsModalOpen(false)}
+/>
     </div>
   );
 };
