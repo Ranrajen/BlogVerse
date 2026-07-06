6e7e6506b1b957c0c0f8a7f4967ce97b58617c70
Ranrajen <rajenrangari863@gmail.com>
2026-07-06 09:20:46 +0530

    added new blog pop up

---
+++ src/components/newpostmodal/NewPostModal.tsx
@@ -0,0 +1,42 @@
+import './newPostModal.css'
+
+interface NewPostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const NewPostModal = ({
+  isOpen,
+  onClose,
+}: NewPostModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+
+        <div className="modal-header">
+          <h2>Create New Post</h2>
+
+          <button
+            className="close-btn"
+            onClick={onClose}
+          >
+            ✕
+          </button>
+        </div>
+
+        <div className="modal-body">
+
+          <p>
+            Form coming in the next sprint...
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default NewPostModal;
