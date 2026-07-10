4a79baf9e27a0a567560ee08d7aab04dcd6d9f78
Ranrajen <rajenrangari863@gmail.com>
2026-07-10 10:09:13 +0530

    edit option working done.. on post feed and card

--- src/components/postcard/PostCard.tsx
+++ src/components/postcard/PostCard.tsx
@@ -4,9 +4,10 @@ interface PostCardPost {
   post: Post;
   onSelect : (post : Post )=> void;
   onDelete : (id :string)=> void ;
+  onEdit: (post: Post) => void;
 }
 
-export const Postcard = ({ post , onSelect ,onDelete }: PostCardPost) => {
+export const Postcard = ({ post , onSelect ,onDelete, onEdit }: PostCardPost) => {
   return (
     <div className="post-card" onClick={()=> onSelect(post)}>
       <h3>{post.title}</h3>
@@ -14,8 +15,23 @@ export const Postcard = ({ post , onSelect ,onDelete }: PostCardPost) => {
         {post.author} * {post.createdAt}
       </p>
       <p className='post-category'>{post.category}</p>
-      <button className="delete-btn" onClick={()=> onDelete(post.id)}>
+    <button
+  className="delete-btn"
+  onClick={(e) => {
+    e.stopPropagation();
+    onDelete(post.id);
+  }}
+>
   Delete
+</button>
+<button
+  className="edit-btn"
+  onClick={(e) => {
+    e.stopPropagation();
+    onEdit(post);
+  }}
+>
+  Edit
 </button>
     </div>
   );
--- src/components/postsfeed/PostsFeed.tsx
+++ src/components/postsfeed/PostsFeed.tsx
@@ -7,13 +7,14 @@ interface PostsFeedProps {
   posts : Post[];
   onSelectPost : (post :Post)=> void;
     onDeletePost: (id: string) => void;
+     onEditPost: (post: Post) => void;
 }
 
-const PostsFeed = ({posts, onSelectPost  , onDeletePost}: PostsFeedProps) => {
+const PostsFeed = ({posts, onSelectPost  , onDeletePost , onEditPost,}: PostsFeedProps) => {
   return (
     <section className='post-feed'>
         <h2>POSTS</h2>
-       {posts.map((post)=>(<Postcard key={post.id} post={post} onSelect={onSelectPost} onDelete={onDeletePost}/>))} 
+       {posts.map((post)=>(<Postcard key={post.id} post={post} onSelect={onSelectPost} onDelete={onDeletePost} onEdit={onEditPost}/>))} 
        {/* here post var */}
     </section>
   )
